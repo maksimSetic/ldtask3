79ef3fabb172b0196da8ad4a8a190398a244a84c
Maksim <seticmaksim@gmail.com>
2024-05-22 22:56:00 +0200

    Initial commit - Almost done

---
+++ index.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper(".swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1: { slidesPerView: 1, spaceBetween: 0 },
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 60,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 60,
+    },
+    880: {
+      slidesPerView: 3,
+      spaceBetween: 60,
+    },
+  },
+});
